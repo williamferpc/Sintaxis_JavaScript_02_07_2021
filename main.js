95115a9c803ae948197b1485ddda0925d8156f74
williamferpc <willi_9212@hotmail.com>
2021-07-19 17:57:37 -0500

    sintaxis javascript 02_07_2021

---
+++ main.js
@@ -0,0 +1,99 @@
+
+
+nombreString = `WILLIAM FERNANDO PEÑA CORZO`;
+edadNumber = 28;
+soyProfesorBolena = false;
+estudiantesAprobadosDiplomadoNull = null;
+cocerUndefined = undefined;
+hobbiesArray = ["Programar","jugar videojuegos", 3157552287, null];
+hobbiesArray["Soy profesor"] = soyProfesorBolena;
+dirrecionPersonalObjecte = {
+    dirrecion: "Carrera 56 w",
+    pais: "Colombia",
+    Pais: "Colombia2",
+    mundo: "Tierra",
+    0: "Hola soy el identificador numerico"
+};
+
+console.log(hobbiesArray);
+console.log(dirrecionPersonalObjecte);
+console.log(`Datos de la variable tipo Array: `,hobbiesArray["Soy profesor"], hobbiesArray[0]);
+console.log(`Datos de la variable tipo Object: `,dirrecionPersonalObjecte.pais, dirrecionPersonalObjecte[0]);
+
+
+
+
+
+
+
+
+
+
+
+// num1 = 50;
+// num2 = 3;
+
+// tipoDeDato = typeof(num1);
+
+
+// console.log(`
+//     Numero 1 = ${num1} 
+//     Numero 2 = ${num2}
+//     Resultado de la suma = ${num1 + num2}
+// `);
+
+
+
+
+
+
+
+// console.log(`%c
+//     Numero 1 = ${num1} 
+//     Numero 2 = ${num2}
+//     Resultado de la suma = ${num1 + num2}
+// `,"color:orange; font-family:cursive;");
+
+// console.log(`
+//     Numero 1 = ${num1} 
+//     Numero 2 = ${num2}
+//     Resultado de la suma = ${num1 + num2}
+// `);
+// console.error(`
+//     Numero 1 = ${num1} 
+//     Numero 2 = ${num2}
+//     Resultado de la suma = ${num1 + num2}
+// `);
+// console.info(`
+//     Numero 1 = ${num1} 
+//     Numero 2 = ${num2}
+//     Resultado de la suma = ${num1 + num2}
+// `);
+// console.warn(`
+//     Numero 1 = ${num1} 
+//     Numero 2 = ${num2}
+//     Resultado de la suma = ${num1 + num2}
+// `);
+
+// document.write(`
+//     Numero 1 = ${num1} 
+//     Numero 2 = ${num2}
+//     Resultado de la suma = ${num1 + num2}
+// `);
+
+// alert(`
+//     Numero 1 = ${num1} 
+//     Numero 2 = ${num2}
+//     Resultado de la suma = ${num1 + num2}
+// `);
+
+
+
+
+
+
+
+
+// console.log("El resultado de la suma es: "+suma);
+// console.log('El resultado de la suma es: '+suma);
+// console.log('El resultado de la suma es: ',suma);
